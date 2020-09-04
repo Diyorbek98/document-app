b2b2b0caec3e94ccb60b15dfbf948129e94a880b
Sa'dullayevDQ <Diyorbek98>
2020-09-05 00:51:08 +0500

    2-commit

--- document-app-server/src/main/java/uz/diyorbek/documentappserver/repository/CorrespondentRepository.java
+++ document-app-server/src/main/java/uz/diyorbek/documentappserver/repository/CorrespondentRepository.java
@@ -3,7 +3,8 @@ package uz.diyorbek.documentappserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uz.diyorbek.documentappserver.entity.Correspondent;
+import uz.diyorbek.documentappserver.projection.CustomCorrespondent;
 
-@RepositoryRestResource(path = "correspondent")
+@RepositoryRestResource(path = "correspondent", excerptProjection = CustomCorrespondent.class)
 public interface CorrespondentRepository extends JpaRepository<Correspondent, Integer> {
 }
--- document-app-server/src/main/java/uz/diyorbek/documentappserver/repository/DeliveryRepository.java
+++ document-app-server/src/main/java/uz/diyorbek/documentappserver/repository/DeliveryRepository.java
@@ -3,7 +3,8 @@ package uz.diyorbek.documentappserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uz.diyorbek.documentappserver.entity.Delivery;
+import uz.diyorbek.documentappserver.projection.CustomDelivery;
 
-@RepositoryRestResource(path = "delivery")
+@RepositoryRestResource(path = "delivery", excerptProjection = CustomDelivery.class)
 public interface DeliveryRepository extends JpaRepository<Delivery, Integer> {
 }
